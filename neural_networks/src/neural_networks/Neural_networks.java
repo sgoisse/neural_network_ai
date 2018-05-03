0088a67dd8171d56d8f2a1e7442231c976626384
DESKTOP-9CC45F6\simon <simon.goisse@gmail.com>
2018-05-03 12:56:45 +0200

    main classe

---
+++ neural_networks/src/neural_networks/Neural_networks.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package neural_networks;
+
+/**
+ *
+ * @author simon
+ */
+public class Neural_networks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
