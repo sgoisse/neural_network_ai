28b8099b7f41fad04b29a07fecd2eeba9418040c
DESKTOP-9CC45F6\simon <simon.goisse@gmail.com>
2018-05-03 13:12:31 +0200

    begin of a neuron

---
+++ neural_networks/src/neural_networks/Neurone.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package neural_networks;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author simon
+ */
+public class Neurone {
+    private int MIN = 1;
+    private int MAX = 40;
+    private ArrayList connection_weight = new ArrayList();
+    private int layer_number;
+    
+    public Neurone(){
+        
+    }
+    
+    
+    
+    public void initialisation_weight(){
+        for(int i=0;i<layer_number;++i){
+            
+            connection_weight.add((int)( Math.random()*( MAX - MIN ) ) + MIN );
+        }
+}
+    
+    
+}
+
+
